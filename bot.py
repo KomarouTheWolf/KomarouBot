5c1bf0b175d56f481a71e027371a72aefd973e20
KomarouTheWolf <98citycafe@gmail.com>
2022-01-21 00:52:54 +0800

    oao

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import json
 
-with open{'setting.json','r',encoding='utf-8'} as jfile:
+with open('setting.json','r',encoding='utf-8') as jfile:
     jdata=json.load(jfile)
 
 bot = commands.Bot(command_prefix='kmrzb')
@@ -20,5 +20,11 @@ async def on_member_join(member):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000,1)}ms')
+
+@bot.command()
+async def picture(ctx):
+    pic=discord.File('C:\\Users\\user\\Documents\\GitHub\\KomarouBot\\pictures\\1.jpg')
+    await ctx.send(file=pic)
+
 
 bot.run(jdata['TOKEN'])
